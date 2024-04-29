06581fae33f185780915f3a3b68f7e0ca35fb970
shestooy <kmd017@campus.mephi.ru>
2024-04-29 11:08:38 +0300

    1.1.1

--- authorization/package.php
+++ authorization/package.php
@@ -11,19 +11,20 @@ if (isset($_COOKIE["error_message"])) {
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <h2>Вход</h2>
-    <form action="login.php" method="post">
-        <label for="username">Логин</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Пароль:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Отправить"><br><br>
-    </form>
-
-    Для новых моих пользователей:
-    <a href="register.php">Регистрация</a><title></title>
-
+    <title>Главная страница</title>
 </head>
+<meta charset="UTF-8">
+<h2>Вход</h2>
+<form action="login.php" method="post">
+    <label for="username">Логин</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Пароль:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    <input type="submit" value="Отправить"><br><br>
+</form>
+
+Для новых моих пользователей:
+<a href="register.php">Регистрация</a><title></title>
+
 </html>
 
